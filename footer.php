34c4b38d473637ceaafed287f099b09c1fa8eb52
Johnny Rivera <johnny1864g@gmail.com>
2026-03-01 18:23:24 -0800

    updates

--- footer.php
+++ footer.php
@@ -2,10 +2,28 @@
     $footer = get_field('footer', 'option');
     $footer_content = $footer['footer_content'];
     if(!empty($footer['site_logo'])) $site_logo = getIMG( $footer['site_logo']['ID'], 'md', false, array('alt' => get_bloginfo( 'name' ), 'lazy' => false));
+
+    $footer_banner = $footer['footer_banner'];
 ?>
 
 </main>
 
+<div class="gfooter__banner section">
+    <div class="container">
+        <div class="flex items-center justify-around gap-5">
+            <div class="gfooter__banner-image w-1/3">
+                <?php if($footer_banner['logo_1']) : ?>
+                    <?php echo getIMG($footer_banner['logo_1']['ID']) ?>
+                <?php endif; ?>
+            </div>
+            <div class="gfooter__banner-image w-1/3">
+                <?php if($footer_banner['logo_2']) : ?>
+                    <?php echo getIMG($footer_banner['logo_2']['ID']) ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
 <footer class="gfooter">
     <div class="container">
         <div class="gfooter__content text-center">
